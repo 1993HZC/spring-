f33330495317c93ce853a9a97504cc6ee01a330b
1993HZC <hzc19930921@163.com>
2020-06-15 21:17:16 +0800

    基于spring xml配置的事务控制

--- src/main/java/com/markov/service/impl/AccountServiceImpl.java
+++ src/main/java/com/markov/service/impl/AccountServiceImpl.java
@@ -40,7 +40,6 @@ public class AccountServiceImpl implements IAccountService{
             //2.5更新转出账户
             accountDao.updateAccount(source);
 
-//            int i=1/0;
 
             //2.6更新转入账户
             accountDao.updateAccount(target);
